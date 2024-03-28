2827927157b03a34fe65c1b4d924affa88676337
khakimovSamdu <xakimov2806@gmail.com>
2024-03-27 18:21:34 -0800

    5-5-dars

---
+++ blog/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .models import Post
+from django.views.generic import ListView, DetailView
+from django.views.generic import CreateView
+# Create your views here.
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'autor', 'body']
